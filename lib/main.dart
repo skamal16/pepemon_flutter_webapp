5ebf7d2f4b3a44bfe6bdd62b1b9a60ee47f49ce8
Syed Anas Kamal <39828893+skamal16@users.noreply.github.com>
2023-06-10 18:33:37 +0500

    In main.dart updated http to https due to security error blockage when deployed

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class _UnityScreenState extends State<UnityScreen> {
   Widget build(BuildContext context) {
     return UnityWebWidget(
       // url: 'http://localhost:${Uri.base.port}/unity/index.html',
-      url: 'http://${Uri.base.host}:${Uri.base.port}/unity/index.html',
+      url: 'https://${Uri.base.host}:${Uri.base.port}/unity/index.html',
       listenMessageFromUnity: _listenMessageFromUnity,
       onUnityLoaded: _onUnityLoaded,
     );
